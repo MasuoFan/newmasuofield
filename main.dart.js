06f8a25fbeb5d621240a16ef7aca2cb76bcf9aca
MasuoFan <kaitekinidekiru@gmail.com>
2026-08-25 00:18:03 +0900

    deleted if

--- main.dart.js
+++ main.dart.js
@@ -5156,8 +5156,7 @@ n.n(0,p,o)
 p=o}else p=o
 J.o0(p,q)}return n},
 tm(){var s=A.a(v.G.window)
-if(B.d.R(A.v(A.a(s.location).hostname),"app")||A.v(A.a(s.location).pathname)!=="/"){A.a(s.location).href="https://google.com"
-return}A.f1()},
+A.f1()},
 O(a){var s=t.N,r=t.z
 return t.f.b(a)?A.a8(a,s,r):A.aq(s,r)},
 lY(a){var s
